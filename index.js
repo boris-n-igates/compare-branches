2a8ba526bce57d30b56a42a4c3f5457dd376430f
Boris Nadysev <boris@igates.co.il>
2022-12-06 11:29:34 +0200

    logs

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const github = require('@actions/github');
 async function run(){
 
   try {
-    const token = core.getInput('github_token');
-    const base = core.getInput('base');
-    const head = core.getInput('head');
+    const token = core.getInput('github_token', {required: true});
+    const base = core.getInput('base',{required: true});
+    const head = core.getInput('head', {required: true});
   
     const context = github.context;
     const client = github.getOctokit(token);
+
+    console.log('base ' + base);
+    console.log('head ' + head);
 
     const [response, status] = await client.request('GET /repos/{owner}/{repo}/compare/{basehead}{?page,per_page}', {
       owner: context.repo.owner,
